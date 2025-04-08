afb43788d695a8ad52e316979f55f2336310ab50
PedroHenrique973 <boxpedrohenrique@gmail.com>
2025-04-08 18:15:15 +0000

    Codigo Pronto

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,99 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int tabuleiro[10][10];  //Tamanho do tabuleiro
+
+void Tabuleiro() {
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void navioHorizontal(int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (coluna + i <= 9)
+            tabuleiro[linha][coluna + i] = 3;
+    }
+}
+
+void navioVertical(int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (linha + i <= 9)
+            tabuleiro[linha + i][coluna] = 3;
+    }
+}
+
+void navioDiagonalPrincipal(int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (linha + i <= 9 && coluna + i <= 9)
+            tabuleiro[linha + i][coluna + i] = 3;
+    }
+}
+
+void navioDiagonalInversa(int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (linha + i <= 9 && coluna - i >= 0)
+            tabuleiro[linha + i][coluna - i] = 3;
+    }
+}
+
+//Cone = 6, Cruz = 4, Octaedro = 5
+void habilidadeCone(int linha, int coluna) {
+    for (int i = 0; i <= 9; i++) {
+        for (int j = coluna - i; j <= coluna + i; j++) {
+            if (j >= 0 && j <= 9 && tabuleiro[i][j] == 0)
+                tabuleiro[i][j] = 6;
+        }
+    }
+}
+
+void habilidadeCruz(int linha, int coluna) {
+    for (int i = 0; i <= 9; i++) {
+        if (tabuleiro[linha][i] == 0)
+            tabuleiro[linha][i] = 4;
+        if (tabuleiro[i][coluna] == 0)
+            tabuleiro[i][coluna] = 4;
+    }
+}
+
+int valorabsoluto(int x) {
+    if (x < 0)
+        return -x;
+    else
+        return x;
+}
+
+void habilidadeOctaedro(int linha, int coluna) {
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            int distancia = valorabsoluto(i - linha) + valorabsoluto(j - coluna);
+            if (distancia <= 2 && tabuleiro[i][j] == 0)
+                tabuleiro[i][j] = 5;
+        }
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Inicializar tabuleiro
+    for (int i = 0; i <= 9; i++)
+        for (int j = 0; j <= 9; j++)
+            tabuleiro[i][j] = 0;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //navios
+    navioHorizontal(1, 6);
+    navioVertical(5, 5);
+    navioDiagonalPrincipal(1, 1);
+    navioDiagonalInversa(7, 8);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //habilidades
+    habilidadeCone(3, 5);
+    habilidadeCruz(5, 5);
+    habilidadeOctaedro(6, 4);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("Tabuleiro:\n");
+    Tabuleiro();
 
     return 0;
 }
